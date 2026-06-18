9f4199a73337b6deade30377010f0e374dfac913
newman05 <namansurana4566@gmail.com>
2026-06-18 08:55:57 +0530

    config

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+export const WRITE_ONLY_ON_CHANGE = 
+  process.env.MONITOR_WRITE_ONLY_ON_CHANGE === 'true';
+
+export const FORCE_WRITE_INTERVAL = 
+  parseInt(process.env.MONITOR_FORCE_WRITE_INTERVAL || '6', 10);
